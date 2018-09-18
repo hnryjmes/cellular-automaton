1682c31551be5f30998d81646cd06b63f381d8d8
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-18 16:39:50 +0100

    basic layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Grid extends React.Component {
+  render() {
+    return (
+      <div>
+        grid
+      </div>
+    );
+  }
+}
+
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>cellular automaton</h1>
+        <Grid
+        />
+        <h2>generations: {this.state.generation}</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
