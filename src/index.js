939e3e08954b017954972b0ba22784919597a221
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-20 14:41:53 +0100

    seeded grid

--- src/index.js
+++ src/index.js
@@ -71,6 +71,24 @@ class Main extends React.Component {
     });
   }
 
+  seed = () => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        if (Math.floor(Math.random() * 4) === 1) {
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      gridFull: gridCopy
+    });
+  }
+
+  componentDidMount() {
+    this.seed();
+  }
+
   render() {
     return (
       <div>
